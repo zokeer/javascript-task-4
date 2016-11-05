d27cad96ff4083198abe58d29e535886b5b25a74
Dmitry Golubev <zokeerxd@gmail.com>
2016-11-05 22:33:01 +0500

    corrected mistakes

--- lego.js
+++ lego.js
@@ -2,19 +2,16 @@
 
 exports.isStar = false;
 var Priors = {
-    'filterIn': 5,
-    'select': 3,
-    'sortBy': 4,
-    'format': 1,
-    'limit': 2
+    filterIn: 5,
+    select: 3,
+    sortBy: 4,
+    format: 1,
+    limit: 2
 };
 
 exports.query = function (collection) {
     var friends = JSON.parse(JSON.stringify(collection));
-    var funcList = [];
-    for (var i = 1; i < arguments.length; i++) {
-        funcList.push(arguments[i]);
-    }
+    var funcList = Array.from(arguments).slice(1);
     funcList.sort(function (one, another) {
         return Priors[another.name] - Priors[one.name];
     });
@@ -46,33 +43,28 @@ exports.select = function () {
 
 exports.filterIn = function (property, values) {
     return function filterIn(collection) {
-        collection = collection.filter(function (elem) {
+        return collection.filter(function (elem) {
             if (elem.hasOwnProperty(property)) {
                 return values.indexOf(elem[property]) !== -1;
             }
 
             return false;
         });
-
-        return collection;
     };
 };
 
 
 exports.sortBy = function (property, order) {
+    var sign = { 'asc': 1, 'desc': -1 };
+
     return function sortBy(collection) {
-        collection.sort(function (one, another) {
+        return collection.sort(function (one, another) {
             if (one.hasOwnProperty(property) && another.hasOwnProperty(property)) {
-                return one[property] > another[property] ? 1 : -1;
+                return sign[order] * (one[property] > another[property] ? 1 : -1);
             }
 
             return 0;
         });
-        if (order === 'desc') {
-            return collection.reverse();
-        }
-
-        return collection;
     };
 };
 
